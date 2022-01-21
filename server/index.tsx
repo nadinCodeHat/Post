667e342752a9925dc82e66c77b57952544193789
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2022-01-21 11:33:40 +0530

    server init

---
+++ server/index.tsx
@@ -0,0 +1,10 @@
+//body-parser -> To send POST requests
+import bodyParser from 'body-parser';
+//cors -> For cross origin requests
+import cors from 'cors';
+//express -> Express as a framework for creating the routing of the application
+import express from 'express';
+//mongoose -> To create models for the posts
+import mongoose from 'mongoose';
+//nodemon -> So no need to manually reset the server everytime a change is made instead nodemon will do it
+
