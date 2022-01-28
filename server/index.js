362c2036a3da9415ca33b2007f10678880a25a1e
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2022-01-28 21:49:12 +0530

    bug fix

--- server/index.js
+++ server/index.js
@@ -13,11 +13,12 @@ import postRoutes from './routes/posts.js';
 //initialize app
 const app = express();
 
-app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://nadinCodeHat:123@cluster0.so9qo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
