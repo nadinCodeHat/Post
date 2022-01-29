01447b8cedc000802f6d93c2802884dde1d2da3b
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2022-01-29 18:18:17 +0530

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,18 @@
-//axios -> For making API requests
-//moment -> (moment js) libary to work with time and date
-//react-file-base64 -> To convert images
-//redux
-//redux-thunk -> For asynchronous actions using redux
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import reducers from './reducers';
+
+import { reducers } from './reducers';
 import App from './App';
+import './index.css';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
 );
--- server/index.js
+++ server/index.js
@@ -1,33 +1,24 @@
-//body-parser -> To send POST requests
-import bodyParser from 'body-parser';
-//cors -> For cross origin requests
-import cors from 'cors';
-//express -> Express as a framework for creating the routing of the application
+
 import express from 'express';
-//mongoose -> To create models for the posts
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-//nodemon -> So no need to manually reset the server everytime a change is made instead nodemon will do it
+import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
 
-//initialize app
 const app = express();
 
-
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://nadinCodeHat:123@cluster0.so9qo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log('Server running on port: ${PORT}')))
-    .catch((error) =>  console.log(error.message));
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
-//mongoose('useFindAndModify', false);
-
-//connect app with database (mongodb)
- 
+mongoose.set('useFindAndModify', false);
